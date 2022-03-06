ffc4322decf160b2dcae181c5a34d3466868d4e3
Sakib Rahman <rahmans@myumanitoba.ca>
2022-03-06 01:05:18 -0600

    Plot histograms in photon tracking macro

--- plot/rate_track_MD_photon/trackPhotons1D.C
+++ plot/rate_track_MD_photon/trackPhotons1D.C
@@ -4,7 +4,7 @@ Float_t scale = 1.0/nFiles;
 
 TString pid = "photon";
   
-std::vector<TString>  det;
+std::vector<TString>  detector;
 det.push_back("Col2Ent");
 det.push_back("Col2Exit");
 det.push_back("USCoil1");
@@ -22,9 +22,26 @@ det.push_back("DSCoil3");
 det.push_back("DSCoil6");
 det.push_back("DSCoil10");
 det.push_back("MD");
+
+THStack hs;
+TLegend *l=new TLegend(0.6,0.6,0.9,0.9);
+TFile *f =new TFile(Form("%s/beam.root", loc.Data()));
   
+for(Int_t k=0; k<detector.size(); k++){
+ part= Form("pr_%s_%s_%s_%s", detector[k].Data(), sector.Data(), pid.Data(), suff.Data());
+ TH1F* h= (TH1F*) ((TDirectory*) f->Get(det))->Get(part);
+ h->SetDirectory(0);
+ l->AddEntry(h, detector[k]);
+ h->Scale(scale);
+ h->SetLineColor(k+1);
+ h->SetLineWidth(2);
+ hs.Add(h);
+}
 
-
+hs.Draw("HIST");
+gPad->SetLogy();
+hs.SetTitle("Radial Distribution (1/e/5mm)");
+c->Print(loc+"/summary.png");
   
 return 0;
 }
