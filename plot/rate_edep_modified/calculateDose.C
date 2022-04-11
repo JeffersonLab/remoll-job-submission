dd87eec24e07633397d58f27de760f45f8ed2bf3
Damon Spayde <spayde@hendrix.edu>
2022-04-11 11:43:45 -0400

    Update dose plotting script for increased efficiency
    - Make larger TCanvas
    - Calculate total dose and in all coils (plus uncertainty
    - Remove plot of single coil in own TCanvas
    - New 'batchDose' function that calls 'calculateDose' for left, right,
      and bottom automatically

--- plot/rate_edep_modified/calculateDose.C
+++ plot/rate_edep_modified/calculateDose.C
@@ -1,11 +1,12 @@
 Int_t calculateDose(TString input, TString type, Int_t scale, bool
-    fixed_range=true, Int_t detector=1){
+    fixed_range=false, Int_t detector=1){
 
 TFile *f =new TFile(Form("%s",input.Data()));
 Float_t weight = 65*344*24*60*60/(40e-9*1.3*1e3*1e6);
 
-TCanvas *c=new TCanvas("c","c", 800, 600);
-c->SetMargin(0.15,0.15, 0.15, 0.15);
+TCanvas *c=new TCanvas("c","c", 1200, 900);
+/* TCanvas *c=new TCanvas("c","c", 800, 600); */
+/* c->SetMargin(0.15,0.15, 0.15, 0.15); */
 std::map<Int_t,  std::vector<TH2D*>> h_clone;
 
 TString out="";
@@ -50,43 +51,61 @@ levels[1]=50.0;
 levels[2]=80.0;
 levels[3]=160.0;
 
+vector<Double_t> coil_dose;
+
 c->Divide(3,3); 
- for(Int_t i=1;i<=7;i++){
-c->cd(i);
-gPad->SetMargin(0.15,0.15, 0.15, 0.15);
-h_clone[i][0]->SetTitle("Dose(MGy)");
-h_clone[i][0]->SetTitleSize(0.06);
-/* if ( fixed_range == kTrue ) { */
-/*   h_clone[i][0]->SetContour(nlevels, levels); */
-/* } */
-h_clone[i][0]->Draw("colz");
-if ( fixed_range == true ) {
-  h_clone[i][0]->SetContour(nlevels, levels);
-  h_clone[i][0]->GetZaxis()->SetRangeUser(0,200);
-}
-h_clone[i][0]->GetZaxis()->SetLabelSize(0.06);
-h_clone[i][0]->GetYaxis()->SetLabelSize(0.06);
-h_clone[i][0]->GetXaxis()->SetLabelSize(0.06);
-h_clone[i][0]->GetXaxis()->SetRangeUser(5000,12000);
-cout << "Coil " << i << " Total Dose = " << h_clone[i][0]->Integral();
-cout << " MGy\n";
+for(Int_t i=1;i<=7;i++){
+  c->cd(i);
+  gPad->SetMargin(0.15,0.15, 0.15, 0.15);
+  h_clone[i][0]->SetTitle("Dose(MGy)");
+  h_clone[i][0]->SetTitleSize(0.06);
+  /* if ( fixed_range == kTrue ) { */
+  /*   h_clone[i][0]->SetContour(nlevels, levels); */
+  /* } */
+  h_clone[i][0]->Draw("colz");
+  if ( fixed_range == true ) {
+    h_clone[i][0]->SetContour(nlevels, levels);
+    h_clone[i][0]->GetZaxis()->SetRangeUser(0,200);
+  }
+  h_clone[i][0]->GetZaxis()->SetLabelSize(0.06);
+  h_clone[i][0]->GetYaxis()->SetLabelSize(0.06);
+  h_clone[i][0]->GetXaxis()->SetLabelSize(0.06);
+  h_clone[i][0]->GetXaxis()->SetRangeUser(5000,12000);
+  /* cout << "Coil " << i << " Total Dose = " << h_clone[i][0]->Integral(); */
+  /* cout << " MGy\n"; */
+  coil_dose.push_back(h_clone[i][0]->Integral());
+  cout << i << " : " << type.Data() << " : " << h_clone[i][0]->Integral() << "\n";
 } 
+Double_t coil_mean = TMath::Mean(coil_dose.begin(), coil_dose.end());
+Double_t coil_sd = TMath::StdDev(coil_dose.begin(), coil_dose.end());
+Double_t coil_err = coil_sd/TMath::Sqrt(7);
+cout << "Average : " << type.Data() << " : " << coil_mean << "\n";
+cout << "Uncertainty : " << type.Data() << " : " << coil_err << "\n";
 c->Print(Form("%s.png", type.Data()));
 
 
-TCanvas *c1=new TCanvas();
-h_clone[detector][0]->Draw("colz");
-TLine *l=new TLine(5937.67,0,5937.67, 300);
-TLine *l1=new TLine(6972.42,0,6972.42, 300);
-TLine *l2=new TLine(7964.635,0,7964.35, 300);
-l->Draw();
-l1->Draw();
-l2->Draw();
-c1->Print(Form("%s_1.png", type.Data()));
+/* TCanvas *c1=new TCanvas(); */
+/* h_clone[detector][0]->Draw("colz"); */
+/* TLine *l=new TLine(5937.67,0,5937.67, 300); */
+/* TLine *l1=new TLine(6972.42,0,6972.42, 300); */
+/* TLine *l2=new TLine(7964.635,0,7964.35, 300); */
+/* l->Draw(); */
+/* l1->Draw(); */
+/* l2->Draw(); */
+/* c1->Print(Form("%s_1.png", type.Data())); */
 
-std::cout<<out<<std::endl;
+/* std::cout<<out<<std::endl; */
 
 
 return 0;
 
+}
+
+Int_t batchDose(TString input, Int_t scale, bool fixed_range=false){
+  
+  calculateDose(input, "de_phz_bottom", scale, fixed_range);
+  calculateDose(input, "de_rz_left", scale, fixed_range);
+  calculateDose(input, "de_rz_right", scale, fixed_range);
+
+  return 0;
 }
