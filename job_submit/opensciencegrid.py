afa3baf0221dcbe580dfa87e724199aba656b53a
Sakib Rahman <rahmans@myumanitoba.ca>
2023-02-08 16:49:03 -0600

    Test parameter reading

--- job_submit/opensciencegrid.py
+++ job_submit/opensciencegrid.py
@@ -17,6 +17,7 @@ with open(configfile) as csvfile:
     else:
       p[row[0]]=float(row[1])
 
-condor_sim_file=open(p["condor_sim_file"], "w")
-condor_sim_file.write("test")               
+condor_sim_file=open(p["condor_submit_file"], "w")
+for i in p["remollana_type"]:
+  condor_sim_file.write(i)               
 condor_sim_file.close()
