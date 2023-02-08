2e888126f16c3a2beffb2f239b9daaa1a5bb08fe
Sakib Rahman <rahmans@myumanitoba.ca>
2023-02-08 13:36:29 -0600

    Read parameter values from a config file and allow for special cases with string parameter values

--- job_submit/opensciencegrid.py
+++ job_submit/opensciencegrid.py
@@ -1,9 +1,19 @@
 import sys
 import os
 import re
-
+import csv
 
 configfile = sys.argv[1]
+
+p={}    # dictionary of parameter values
+
+with open(configfile) as csvfile:
+  reader=csv.reader(csvfile, delimiter=',', quotechar='|')
+  for row in reader:
+    if row[2].strip()=="string":
+      p[row[0]]=row[1].strip()
+    else:
+      p[row[0]]=float(row[1])
 
 condor_sim_file=open(p["condor_sim_file"], "w")
 condor_sim_file.write("test")               
