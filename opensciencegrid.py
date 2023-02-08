3675db22b12b6507b2745289b2f7e4ff53f0d5c3
Sakib Rahman <rahmans@myumanitoba.ca>
2023-02-08 13:03:02 -0600

    Initialize parameter to store condor submit file location

--- opensciencegrid.py
+++ opensciencegrid.py
@@ -1,3 +1,10 @@
 import sys
 import os
 import re
+
+
+configfile = sys.argv[1]
+
+condor_sim_file=open(p["condor_sim_file"], "w")
+condor_sim_file.write("test")               
+condor_sim_file.close()
